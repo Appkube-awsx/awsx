d81afd038fb24e9d3bf8c9572dab15fe201828b7
ManojSynectiks <manoj.sharma@synectiks.com>
2023-01-12 23:05:00 +0530

    main awsx command

---
+++ go.mod
@@ -0,0 +1,15 @@
+module awsx
+
+go 1.19
+
+require (
+	github.com/Appkube-awsx/appconfig v1.0.0
+	github.com/spf13/cobra v1.6.1
+)
+
+require (
+	github.com/aws/aws-sdk-go v1.44.178 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/Appkube-awsx/appconfig/cmd"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+)
+
+var AwsxCmd = &cobra.Command{
+	Use:   "awsx",
+	Short: "awsx main command",
+	Long:  `awsx main command`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func Execute() {
+	err := AwsxCmd.Execute()
+	if err != nil {
+		log.Fatal("There was some error while executing the CLI: ", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application. Global means available in child/sub commands
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.appconfig.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	AwsxCmd.AddCommand(cmd.AppconfigCmd)
+	AwsxCmd.PersistentFlags().String("vaultUrl", "", "vault end point")
+	AwsxCmd.PersistentFlags().String("ac", "", "aws account number")
+	AwsxCmd.PersistentFlags().String("region", "", "aws region")
+	AwsxCmd.PersistentFlags().String("accessKey", "", "aws access key")
+	AwsxCmd.PersistentFlags().String("secretKey", "", "aws secret key")
+	AwsxCmd.PersistentFlags().String("crossAccountRoleArn", "", "aws cross account role arn")
+
+}
+
+func main() {
+	cmd.Execute()
+}
